43255ef3149c98dfa553e2887d6f3432d13ab06f
dongyoon7212 <dongyoon7212@naver.com>
2023-11-16 16:53:54 +0900

    소개 글 수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -14,11 +14,10 @@ export default function Hero() {
           맘껏 구경해요!
         </h1>
         <p className="mb-8 leading-relaxed">
-          저는 다양한 프로젝트를 경험하며 성장하고, 새로운 기술에 도전하며
-          스스로를 발전시키는 것을 즐겨요! 그리고 항상 꿈을 키우고 그 꿈을
-          현실로 만들기 위해 노력하고 있는 열정적인 개발자입니다. <br />
+          저는 항상 꿈을 키우고 그 꿈을 현실로 만들기 위해 노력하고 있는
+          열정적인 개발자입니다. <br />
           <br />
-          {"\"오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아간다.\""}
+          {'"오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아간다."'}
         </p>
         <div className="flex justify-center">
           <Link href="/projects" className="btn-project">
--- pages/about-me.js
+++ pages/about-me.js
@@ -28,30 +28,41 @@ export default function AboutMe() {
                 </p>
 
                 <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6 md:text-left">
-                  더 큰 세상을 나아갈 준비 완료
+                  더 큰 바다로 항해할 준비 완료
                 </h1>
 
-                <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
-                  저는 사용자들에게 제가 만든 웹이나 어플 등을 보여주고 사용하는
-                  것에 흥미를 느껴서 코딩에 관심을 가지기 시작했습니다. 처음에는
-                  HTML, CSS로 시작하여 간단한 코드로도 웹을 구현하는 것에 재미를
-                  느꼈고 JavaScript 그리고 더 나아가 React.js까지 공부를
-                  하였습니다. 하지만 공부에 그치지 않고 다른 누군가와 협업을
-                  통해 백엔드에 발을 담가보기도 하고 소통을 하며 나의 프로젝트에
-                  숨을 불어넣는 과정이 짜릿했습니다. 중간중간 난관을 맞이하곤
-                  했지만 포기하지 않고 끝까지 고민한 끝에 해결을 하고 프로젝트가
-                  완성됨으로써 저도 완성이 되는 느낌을 받았습니다. 저는 제
-                  프로젝트의 완성도가 제 자신의 성장률과 비례한다고 생각합니다.
-                  그래서 저는 끊임없이 배우고 성장할 준비가 된 열정적인
-                  개발자입니다.
+                <div className="mb-6 text-gray-500 sm:text-lg md:mb-8">
+                  제가 가지고 있는 강점은 하나를 깊게 파는 것
                   <br />
-                </p>
+                  <br />
+                  <p className="dark:text-white text-black font-semibold text-2xl">
+                    {'"One By One, Deep Dive"'}
+                  </p>
+                  <br />
+                  하나를 깊게 파는 것은 깊은 의미를 갖습니다. 목표를 달성하고자
+                  할 때, 그 목표에 완전히 몰두함으로써 직성이 풀리며, 나
+                  자신에게 진정한 만족감을 안겨줍니다. 제게 있어 &apos;하나만을
+                  택한다&apos; 또는 &apos;과도한 집중&apos;이라는 지적을 받은
+                  적이 있지만, 오히려 이러한 성향을 최대한 활용하여 성공적인
+                  프로젝트를 구상하고 추진했습니다. 나의 강점으로 이를 살려내어,
+                  깊게 파고든 결과물들이 바로 제 프로젝트들입니다.
+                  <br />
+                  그 어떠한 난관을 맞이 하더라도 내가 하고자 한다면 끝까지 깊게
+                  파서 해결을 합니다.
+                  <br />
+                  <br />
+                  <p className="dark:text-white text-black font-semibold text-2xl">
+                    “바다처럼 깊고 넓은 성장가능성”
+                  </p>
+                  <br />
+                  오늘은 무엇에 몰두해보셨나요?
+                </div>
 
                 <h2 className="mb-2 text-center text-xl font-semibold text-gray-800 sm:text-2xl md:mb-4 md:text-left">
                   About Me
                 </h2>
 
-                <div className="">
+                <div>
                   <h2>이름</h2>
                   <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
                     이동윤
